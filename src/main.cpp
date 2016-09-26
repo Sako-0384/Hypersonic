7be530712c4b0ac65e13429effd0adce4aceab54
nonsako <freakeywiper@gmail.com>
2016-09-27 02:31:44 +0900

    りせっと♪

--- src/main.cpp
+++ src/main.cpp
@@ -10,5 +10,29 @@ const int dx[4] = {1,0,-1,0};
 const int dy[4] = {0,1,0,-1};
 
 int main() {
+    int width;
+    int height;
+    int myId;
+    cin >> width >> height >> myId; cin.ignore();
 
+    // game loop
+    while (1) {
+        for (int i = 0; i < height; i++) {
+            string row;
+            cin >> row; cin.ignore();
+        }
+        int entities;
+        cin >> entities; cin.ignore();
+        for (int i = 0; i < entities; i++) {
+            int entityType;
+            int owner;
+            int x;
+            int y;
+            int param1;
+            int param2;
+            cin >> entityType >> owner >> x >> y >> param1 >> param2; cin.ignore();
+        }
+
+        cout << "BOMB 6 5" << endl;
+    }
 }
